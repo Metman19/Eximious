227c8e307898af04e387da3f45abbf373d0cfe7d
Gio <ggalvis@1sale.com>
2017-04-16 00:48:00 -0400

    new changes

--- loginapp/config/auth.js
+++ loginapp/config/auth.js
@@ -4,4 +4,9 @@ module.exports = {
     clientSecret: 'aaec7d8decdb55377ca96bf5d539d2d8',
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
   },
+  twitterAuth: {
+    consumerKey: 'TJBiw9AiBzxXmt5FG7Jts2KRa',
+    consumerSecret: 'yIzFkJutda97CuP9dlJAVDHL1UQu3gpwkYW1CH7SZVLYQZWzzA',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
 };
--- loginapp/routes/index.js
+++ loginapp/routes/index.js
@@ -16,6 +16,15 @@ router.get('/auth/facebook/callback', passport.authenticate('facebook', {
   failureRedirect: '/',
 }));
 
+// Twitter routes
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
